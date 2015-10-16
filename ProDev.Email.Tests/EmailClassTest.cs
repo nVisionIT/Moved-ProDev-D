7ada00f370aa65744cc358f1ef52807f965d5046
RehanaVanSchalkwyk <rehanav@nvisionit.co.za>
2015-10-16 15:46:56 +0200

    Email Test Fixed

--- ProDev.Email.Tests/EmailClassTest.cs
+++ ProDev.Email.Tests/EmailClassTest.cs
@@ -6,9 +6,11 @@ namespace ProDev.Email.Tests
     [TestClass]
     public class EmailClassTest
     {
+        
         [TestMethod]
         public void TestClass(Email email)
         {
+             email = new Email() { To = "rehanav@nvisionit.co.za", Subject = "Unit Test", DeliveryType = "Email", Message = "Test" };
             Assert.IsNotNull(email.To);
             Assert.IsNotNull(email.Subject);
 
